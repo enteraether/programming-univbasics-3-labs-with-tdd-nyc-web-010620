489f1370df4f8457952ce88cdbdc96458f37fe18
Theresa <thetheresagarcia@gmail.com>
2019-12-06 03:00:48 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -5,7 +5,17 @@ def first_number(firstnum)
 end
 
 def second_number(secnum)
+  if secnum == 0
+    puts "enter a nonzero number"
+  end
   secnum = gets.to_i
 end
 
 sum = #{firstnum} + #{secnum}
+
+difference = #{firstnum} - #{secnum}
+
+product = #{firstnum} * #{secnum}
+
+quotient = #{firstnum} / #{secnum}
+# do int.0 to make it a float?
