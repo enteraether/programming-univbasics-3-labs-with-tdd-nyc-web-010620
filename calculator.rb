67bea5bdaed4a9c7a997430ada7f8815ebbb2783
Theresa <thetheresagarcia@gmail.com>
2019-12-06 02:30:58 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,8 +1,8 @@
 def first_number(firstnum)
-  firstnum.get.to_i  
-end  
+  firstnum.get.to_i
+end
 
 def second_number(secnum)
-  secnum.get.to_i 
-  if secnum == 
+  secnum.get.to_i
+  if secnum ==
 end  
